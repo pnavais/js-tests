e2d9729b5c09456a512b7314e06e034408b656a6
Pablo Navais <pnavais@gmail.com>
2017-04-07 15:37:47 +0200

    Added GamePrice class

---
+++ src/GamePrice.js
@@ -0,0 +1,55 @@
+
+function GamePrice(gameId)  {
+	this._gameId = gameId;
+}
+
+
+GamePrice.prototype.setOrder = function(order) {
+	this._order = order;
+};
+
+GamePrice.prototype.setSource = function(source) {
+	this._source = source;
+};
+
+GamePrice.prototype.setURL = function(url) {
+	this._url = url;
+};
+
+GamePrice.prototype.setTitle = function(title) {
+	this._title = title;
+};
+
+GamePrice.prototype.getTitle = function() {
+	return this._title;
+};
+
+GamePrice.prototype.setPrice = function(price) {
+	this._price = price;
+};
+
+GamePrice.prototype.getPrice = function() {
+	return this._price;
+};
+
+GamePrice.prototype.hasDiscount = function(discount) {
+	return this._discount;
+};
+
+GamePrice.prototype.setOldPrice = function(oldPrice) {
+	this._oldPrice = oldPrice;
+};
+
+GamePrice.prototype.getOldPrice = function() {
+	return this._oldPrice;
+};
+
+GamePrice.prototype.setDiscount = function(discount) {
+	this._discount = discount;
+};
+
+GamePrice.prototype.getDiscount = function() {
+	return this._discount;
+};
+
+module.exports = GamePrice;
--- src/SteamWishlistParser.js
+++ src/SteamWishlistParser.js
@@ -1,27 +1,70 @@
-const http = require('request');
-const chalk = require('chalk');
-const cheerio = require('cheerio');
-const log = console.log;
+//const request     = require('request');
+const rp          = require('request-promise');
+const chalk       = require('chalk');
+const cheerio     = require('cheerio');
+const ProgressBar = require('progress');
+const GamePrice   = require('./GamePrice');
+const log         = console.log;
 
-String.prototype.chomp = function ()
-{
-    return this.replace(/(\n|\r)+$/, '');
-};
+var bar;
+//String.prototype.chomp = function ()
+//{
+    //return this.replace(/(\n|\r)+$/, '');
+//};
 
 let userName = "pnavais";
 
+var gameList = [];
 
-log(chalk.blue('Retrieving Steam wishlist for ['+userName+']...'));
+
+log(chalk.yellow('Retrieving Steam wishlist for ['+userName+']...\n'));
 
 
-let request = require('request');
-request('http://steamcommunity.com/id/'+userName+'/wishlist', function (error, response, body) {
-    log('error:', error); // Print the error if one occurred
-    log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+var rp_aux = new rp('http://steamcommunity.com/id/'+userName+'/wishlist', function (error, response, body) {
+    //log('error:', error); // Print the error if one occurred
+    //log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     //log('body:', body); // Print the HTML
     let $ = cheerio.load(body);
     $('div[class="wishlistRow "]').each(function(i, elem) {
-        log("Indice ["+(i+1)+"]> "+$(this).attr("id")+" > "+$(this).find('h4').text()+" > |"+$(this).find('div[class="price"]').text().trim().chomp()+"|");
+    	// General game info
+    	var gamePrice = new GamePrice($(this).attr("id"));
+    	gamePrice.setOrder(i+1);
+        gamePrice.setTitle($(this).find('h4').text());
+        
+        // Retrieve price
+        var gameBlock = $(this).find('div[class="gameListPriceData"]');
+        var price = gameBlock.find('div[class="price"]').text().trim();
+        if (!price) {
+            var discountBlock = gameBlock.find('div[class~="discount_block"]');
+            if (discountBlock) {
+               	price = discountBlock.find('div[class="discount_final_price"]').text().trim();    
+               	gamePrice.setOldPrice(discountBlock.find('div[class="discount_pct"]').text().trim());
+               	gamePrice.setDiscount(discountBlock.find('div[class="discount_original_price"]').text().trim());
+            }
+        }
+
+        // No price found
+        if (!price) {
+        	price = "X";
+        }
+
+        gamePrice.setPrice(price);        
+        gameList.push(gamePrice);
+
+       //+" > |"+$(this).find('div[class="price"]').text().trim().chomp()+"|");
     });
 });
+
+
+rp_aux.then(function(parsedBody) {	
+	for (i = 0; i < gameList.length; i++) {
+		var msg = chalk.white("Game["+(i+1)+"/"+gameList.length+"] : ")+chalk.blue(gameList[i].getTitle())+" -> "+chalk.white(gameList[i].getPrice());
+		if (gameList[i].hasDiscount()) {
+			msg += " "+chalk.bgGreen("("+ gameList[i].getOldPrice()+")");
+		}
+
+		log(msg);
+	}
+});
+
 
