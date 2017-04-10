1a587799ac85338d63d8b284e0530f0fe3a55516
Pablo Navais <pnavais@gmail.com>
2017-04-10 18:10:14 +0200

    Added Ascii representation

--- src/GamePrice.js
+++ src/GamePrice.js
@@ -1,3 +1,4 @@
+'use strict';
 
 function GamePrice(gameId)  {
 	this._gameId = gameId;
--- src/SteamWishlistParser.js
+++ src/SteamWishlistParser.js
@@ -1,37 +1,36 @@
-//const request     = require('request');
-const rp          = require('request-promise');
-const chalk       = require('chalk');
-const cheerio     = require('cheerio');
-const ProgressBar = require('progress');
-const GamePrice   = require('./GamePrice');
-const log         = console.log;
+'use strict';
 
-var bar;
-//String.prototype.chomp = function ()
-//{
-    //return this.replace(/(\n|\r)+$/, '');
-//};
+const rp           = require('request-promise');
+const chalk        = require('chalk');
+const cheerio      = require('cheerio');
+const ProgressBar  = require('progress');
+const Table        = require('cli-table2');
+const accounting   = require('accounting');
+const GamePrice    = require('./GamePrice');
+
+const log          = console.log;
+
 
 let userName = "pnavais";
-
 var gameList = [];
 
 
 log(chalk.yellow('Retrieving Steam wishlist for ['+userName+']...\n'));
 
-
+// Define the Promise
 var rp_aux = new rp('http://steamcommunity.com/id/'+userName+'/wishlist', function (error, response, body) {
-    //log('error:', error); // Print the error if one occurred
-    //log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    //log('body:', body); // Print the HTML
+
+    // Parse the response
     let $ = cheerio.load(body);
+
+    // Process each game row
     $('div[class="wishlistRow "]').each(function(i, elem) {
     	// General game info
     	var gamePrice = new GamePrice($(this).attr("id"));
     	gamePrice.setOrder(i+1);
         gamePrice.setTitle($(this).find('h4').text());
         
-        // Retrieve price
+        // Retrieve game price
         var gameBlock = $(this).find('div[class="gameListPriceData"]');
         var price = gameBlock.find('div[class="price"]').text().trim();
         if (!price) {
@@ -49,22 +48,42 @@ var rp_aux = new rp('http://steamcommunity.com/id/'+userName+'/wishlist', functi
         }
 
         gamePrice.setPrice(price);        
-        gameList.push(gamePrice);
-
-       //+" > |"+$(this).find('div[class="price"]').text().trim().chomp()+"|");
+        gameList.push(gamePrice);       
     });
 });
 
 
+// Print the results in an ASCII Table
 rp_aux.then(function(parsedBody) {	
-	for (i = 0; i < gameList.length; i++) {
-		var msg = chalk.white("Game["+(i+1)+"/"+gameList.length+"] : ")+chalk.blue(gameList[i].getTitle())+" -> "+chalk.white(gameList[i].getPrice());
+	
+    var table = new Table({ /*chars: { 'top': '═' , 'top-mid': '┬' , 'top-left': '╔' ,
+                                     'top-right': '╗', 'bottom': '═' , 'bottom-mid': '╧' ,
+                                     'bottom-left': '╚' , 'bottom-right': '╝', 'left': '║',
+                                     'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼', 'right': '║',
+                                     'right-mid': '╢' , 'middle': '│' 
+                                   }, */
+                            style:{ border:['yellow'], header:[] }
+                        });        
+
+    table.push([{colSpan:3,hAlign:'center',content:'Steam Wishlist for ['+chalk.yellow(userName)+"] , "+gameList.length+" "+((gameList.length>1) ? "games" : "game")+" found" }]);
+	
+    var priceSum = 0;
+	for (var i = 0; i < gameList.length; i++) {
+		var priceTxt = gameList[i].getPrice();
+
+        priceSum += accounting.unformat(priceTxt);
+        
+        var amount = "39,99€";
+        var tema = amount.toLocaleString('de-DE',{style:'currency',currency:'USD'});        
+        log("UNFORMAT PRICE >"+tema);
 		if (gameList[i].hasDiscount()) {
-			msg += " "+chalk.bgGreen("("+ gameList[i].getOldPrice()+")");
-		}
+            priceTxt = chalk.green.bold(priceTxt);
+			priceTxt += " "+chalk.bgGreen.bold("["+gameList[i].getOldPrice()+"]");
+		}		
+        table.push( [ i+1, chalk.blue(gameList[i].getTitle()), {hAlign:'center',content:priceTxt} ] );
+    }
+    table.push( [{colSpan:2, content:'Total'}, {hAlign:'center',content: priceSum}]);
 
-		log(msg);
-	}
+
+	log(table.toString());
 });
-
-
