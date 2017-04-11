6ff91fa7a21dd0ea37a7427c9cb28de97e248809
Pablo Navais <pnavais@gmail.com>
2017-04-11 12:07:13 +0200

    Fixed discount output

--- src/GamePrice.js
+++ src/GamePrice.js
@@ -1,3 +1,31 @@
+/**
+ *  Copyright 2017 Payball
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/**
+ * Contains the basic information of a Steam game including
+ * its price.
+ *
+ * @summary   Contains Steam game information
+ *
+ * @link      https://github.com/pnavais/steam-watcher
+ * @since     30/03/2017  
+ * @class
+ * @classdesc Contains basic Steam game information
+ */
+
 'use strict';
 
 function GamePrice(gameId)  {
--- src/SteamWishlistParser.js
+++ src/SteamWishlistParser.js
@@ -1,10 +1,36 @@
 #!/usr/bin/env node
+
+/**
+ *  Copyright 2017 Payball
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/**
+ * Parses the Steam wishlist page of a given user retrieving 
+ * the list of games and prices, displaying them as an ascii table
+ *
+ * @summary   Parses the Steam Wishlist page of a given User
+ *
+ * @link      https://github.com/pnavais/steam-watcher
+ * @since     30/03/2017  
+ */
+
 'use strict';
 
 const rp           = require('request-promise');
 const chalk        = require('chalk');
 const cheerio      = require('cheerio');
-//const ProgressBar  = require('progress');
 const Table        = require('cli-table2');
 const GamePrice    = require('./GamePrice');
 
@@ -13,11 +39,10 @@ const log          = console.log;
 let userName = "pnavais";
 let gameList = [];
 
-
-log(chalk.yellow('Retrieving Steam wishlist for ['+userName+']...\n'));
-
 // Define the Promise
 const rp_aux = new rp('http://steamcommunity.com/id/' + userName + '/wishlist', function (error, response, body) {
+
+    log(chalk.yellow('Retrieving Steam wishlist for ['+userName+']...\n'));
 
     // Parse the response
     let $ = cheerio.load(body);
@@ -36,8 +61,8 @@ const rp_aux = new rp('http://steamcommunity.com/id/' + userName + '/wishlist', 
             const discountBlock = gameBlock.find('div[class~="discount_block"]');
             if (discountBlock) {
                 price = discountBlock.find('div[class="discount_final_price"]').text().trim();
-                gamePrice.setOldPrice(discountBlock.find('div[class="discount_pct"]').text().trim());
-                gamePrice.setDiscount(discountBlock.find('div[class="discount_original_price"]').text().trim());
+                gamePrice.setDiscount(discountBlock.find('div[class="discount_pct"]').text().trim());
+                gamePrice.setOldPrice(discountBlock.find('div[class="discount_original_price"]').text().trim());
             }
         }
 
@@ -51,13 +76,12 @@ const rp_aux = new rp('http://steamcommunity.com/id/' + userName + '/wishlist', 
     });
 });
 
-
 // Print the results in an ASCII Table
 rp_aux.then(function(parsedBody) {	
 	
     const table = new Table({
         chars: { 'top': '═' , 'top-mid': '┬' , 'top-left': '╔' ,
-                                     'top-right': '╗', 'bottom': '═' , 'bottom-mid': '┴' ,
+                                     'top-right': '╗', 'bottom': '═' , 'bottom-mid': '╧' ,
                                      'bottom-left': '╚' , 'bottom-right': '╝', 'left': '║',
                                      'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼', 'right': '║',
                                      'right-mid': '╢' , 'middle': '│'
@@ -71,16 +95,12 @@ rp_aux.then(function(parsedBody) {
 	for (let i = 0; i < gameList.length; i++) {
 		let priceTxt = gameList[i].getPrice();
 
-        //priceSum += accounting.unformat(priceTxt);
-
-		if (gameList[i].hasDiscount()) {
-            priceTxt = chalk.green.bold(priceTxt);
-			priceTxt += " "+chalk.bgGreen.bold("["+gameList[i].getOldPrice()+"]");
+		if (gameList[i].hasDiscount()) {            
+            priceTxt = chalk.gray.bold.strikethrough(gameList[i].getOldPrice())+" -> "+chalk.green.bold(priceTxt);
+            priceTxt += " "+chalk.bgGreen.bold(gameList[i].getDiscount());            
 		}		
         table.push( [ i+1, chalk.blue(gameList[i].getTitle()), {hAlign:'center',content:priceTxt} ] );
     }
-    table.push( [{colSpan:2, content:'Total'}, {hAlign:'center',content: priceSum}]);
-
 
 	log(table.toString());
 });
